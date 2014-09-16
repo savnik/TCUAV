5a71232be29772148dc8f4c734eaa9e891ce2875
savnik <systems@savnik.dk>
2014-09-16 14:56:01 +0200

    Thesis init

--- Matlab/Kabel_model.m
+++ Matlab/Kabel_model.m
@@ -1,3 +1,21 @@
 %% Kabel Model
 % Peter Savnik, s113556
 % DTU
+clear all;
+close all;
+
+% Static paramters
+cableWeightPrMeter = 1; % [Kg/m]
+
+% Dynamic paramters
+cableLength = 1; % [m] amount of cable out
+Xdist = 1; % [m] distance to drone in x axis
+
+% total cable weight in air
+cableWeight = cableWeightPrMeter*cableLength
+
+% A cable follows cosh form, cartenary
+x=-4:0.1:4;
+
+a=1;
+plot(x,a*cosh((x)/a))
